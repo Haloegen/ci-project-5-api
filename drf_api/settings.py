1750fd1eea36332416095a696254a6dc25ce301a
Josh Lee <JoshCliffLee@gmail.com>
2024-05-12 13:12:58 +0000

    added to allowed host

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -69,7 +69,7 @@ DEBUG = 'DEV' in os.environ
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
     'localhost',
-    'https://review-app-drf-b1e1686b7d68.herokuapp.com/'
+    'review-app-drf-b1e1686b7d68.herokuapp.com/'
 ]
 
 
